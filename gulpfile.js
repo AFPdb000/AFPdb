99ecf63d61adfabfd3c10ecfce7b12fb59896075
iammachine <iammachine@users.noreply.github.com>
2015-07-11 00:12:30 +0530

    initial gulpfile.js - commit.
    Building With Gulp, http://gulpjs.com/ and https://github.com/gulpjs/gulp.

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/*!
+ * AFPdb: Antifreeze Proteins (AFPs) Database.
+ * ___________________________________________________
+ * Gulp: The streaming build system.
+ * https://github.com/gulpjs/gulp | http://gulpjs.com/
+ * @author  : Prabhat Kumar, http://prabhatkumar.org/
+ * @date    : 11-July-2015
+ * ___________________________________________________
+ */
