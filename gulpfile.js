8ee00da0eb8e51fe1d410e65a92cce815466681f
iammachine <iammachine@users.noreply.github.com>
2015-07-11 00:16:40 +0530

    update of gulpfile.js - commit.

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,16 @@
  * @date    : 11-July-2015
  * ___________________________________________________
  */
+'use strict';
+// main module.
+var gulp                   = require('gulp');
+// required modules.
+var sass              = require('gulp-sass');
+var concat          = require('gulp-concat');
+var scsslint     = require('gulp-scss-lint');
+var uglify          = require('gulp-uglify');
+var beautify      = require('gulp-beautify');
+var sourcemaps  = require('gulp-sourcemaps');
+var connect        = require('gulp-connect');
+var jade              = require('gulp-jade');
+var htmlmin        = require('gulp-htmlmin');
